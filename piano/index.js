6923c768f5d3668d0bf4635551a1ecc181cd80b8
github username <github email address>
2018-10-02 09:53:42 -0400

    added files

---
+++ piano/index.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', init)
+
+
+function init(){
+  document.addEventListener('keydown' || 'click', (e) => {
+    let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    console.log(e.keyCode)
+
+    if (!audio) return;
+    audio.currentTime = 0
+    audio.play()
+
+  })
+}
